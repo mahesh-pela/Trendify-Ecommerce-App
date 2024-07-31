9dc7182e5c7fab3edbb62d213104556477c908f7
immp-32 <immp3353@gmail.com>
2024-07-31 17:43:36 +0545

    trendify

---
+++ android/app/src/main/kotlin/com/example/trendify/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.trendify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Screens/Login.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
