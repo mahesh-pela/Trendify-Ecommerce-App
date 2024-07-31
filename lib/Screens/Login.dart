663da1bed38b7a30dd9e3de7429f5ef75f69812c
immp-32 <immp3353@gmail.com>
2024-07-31 19:24:11 +0545

    login ui implementing

--- lib/Screens/Login.dart
+++ lib/Screens/Login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:trendify/constants/styles/textThemeStyles.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -10,7 +11,99 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: CupertinoColors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 140),
+            Text('Welcome Back', style: textStyle1(),),
+            SizedBox(height: 5,),
+            Text('Discover Limitless Choices and Unmatched Convenience.', style: textStyle2(),),
+            SizedBox(height: 25,),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Email',
+                hintStyle: TextStyle(color: Colors.black54),
+                prefixIcon: Icon(Icons.email,),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
 
+              ),
+            ),
+            SizedBox(height: 15,),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Password',
+                prefixIcon: Icon(Icons.password_rounded),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.remove_red_eye_outlined, color: Colors.grey.shade700),
+                  onPressed: (){
+
+                  },
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10)
+                )
+              ),
+
+            ),
+            SizedBox(height: 20,),
+            Row(
+              children: [
+                Icon(Icons.check_box_rounded, color: CupertinoColors.activeBlue,),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Text('Remember Me', style: textStyle2()),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 80),
+                  child: Text('Forget Password?', style: textStyleforgetPass()),
+                )
+              ],
+            ),
+            SizedBox(height: 30),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 50,
+              child: ElevatedButton(
+                  onPressed: (){
+              
+                  }, child: Text('Sign In', style: textStyleWhite()),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: CupertinoColors.activeBlue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)
+                )
+              ),),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () { 
+                
+              },child: Text('Create Account', style: textStyle3(),),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    side: BorderSide(
+                      width: 1,
+                      color: Colors.grey.shade400
+                    )
+                  )
+                ),
+                
+              ),
+            )
+          ],
+
+        ),
+      ),
     );
   }
 }
---
+++ lib/constants/styles/textThemeStyles.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+TextStyle textStyle1(){
+  return TextStyle(
+    fontWeight: FontWeight.w900,
+    fontSize: 25,
+    color: Colors.black,
+  );
+}
+
+TextStyle textStyle2(){
+  return TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: Colors.black,
+  );
+}
+
+TextStyle textStyleforgetPass(){
+  return TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w500,
+    color: Colors.blueAccent.shade700
+  );
+}
+
+TextStyle textStyleWhite(){
+  return TextStyle(
+    fontSize: 15,
+    color: Colors.white,
+    fontWeight: FontWeight.w600
+  );
+}
+
+TextStyle textStyle3(){
+  return TextStyle(
+    fontSize: 15,
+    color: Colors.black,
+    fontWeight: FontWeight.w700
+  );
+}
