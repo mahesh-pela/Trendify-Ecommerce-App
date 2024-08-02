5071e2cfcc9f1a0871f0310a84c37b25660e7176
immp-32 <immp3353@gmail.com>
2024-08-02 23:50:32 +0545

    category section

--- lib/Screens/Home/homeScreen.dart
+++ lib/Screens/Home/homeScreen.dart
@@ -31,7 +31,37 @@ class _HomescreenState extends State<Homescreen> {
               Imageslider(),
               SizedBox(height: 12),
               Text('Categories', style: headLines()),
-            ],
+              SizedBox(height: 10),
+
+              SizedBox(
+                height: 100,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 10,
+                    itemBuilder: (context, index){
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5),
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 56,
+                              height: 56,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(100)
+                              ),
+                              child: ClipOval(
+                                child: Image.asset('assets/images/categories/shoes.jpg', fit: BoxFit.cover,),
+                              )
+                              ),
+                            SizedBox(height: 5,),
+                            Text('Shoes', style: textStyle2(),)
+                          ],
+                        ),
+                      );
+                    },
+              )
+              ),
+          ]
           ),
         ),
       ),
