6fd20952322e0fa6c0792b47aceeffb4e1629684
immp-32 <immp3353@gmail.com>
2024-08-09 17:05:09 +0545

    checkout implementation

---
+++ lib/Screens/Cart/checkOutBox.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class CheckOutBox extends StatelessWidget {
+  const CheckOutBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
+
